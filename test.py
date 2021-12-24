08447f32818c6c4aa3dbca87415ac4d330da91a1
KittenCN <owdely@vip.163.com>
2021-12-24 19:52:50 +0800

    temp

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ from mmdet.apis import inference_detector
 from mmdet.apis import show_result_pyplot
  
 # 模型配置文件
-config_file = '/root/mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
+config_file = 'configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
  
 # 预训练模型文件
-checkpoint_file = '/root/mmdetection/work_dirs/faster_rcnn_r50_fpn_1x_coco/latest.pth'
+checkpoint_file = 'work_dirs/result/normal_1x_1000epochs/latest.pth'
  
 # 通过模型配置文件与预训练文件构建模型
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
